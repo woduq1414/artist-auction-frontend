fa4f77a1e63c2d6e2f638ab5a7c18b3313b5b62f
정재엽 <jjy37777@naver.com>
2024-04-09 17:11:51 +0900

    feat : mobile navbar layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,12 @@ export default function RootLayout({
 }) {
   const [lastScrollY, setLastScrollY] = useState(0);
   const [isWhiteNav, setIsWhiteNav] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    // setIsMenuOpen(!isMenuOpen);
+  };
+
+
   useEffect(() => {
 
     const handleScroll = () => {
@@ -31,7 +37,7 @@ export default function RootLayout({
       if (currentScrollY > 100) {
         setIsWhiteNav(true);
       } else {
-        setIsWhiteNav(false); 
+        setIsWhiteNav(false);
       }
       // 마지막 스크롤 위치 업데이트
       setLastScrollY(currentScrollY);
@@ -47,7 +53,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={pretendard.className}>
 
-        <div className={`fixed top-0 z-10 flex flex-no-wrap items-center justify-between w-full h-20 py-8  lg:flex-wrap lg:justify-start lg:py-4
+        {/* <div className={`fixed top-0 z-[90] flex flex-no-wrap items-center justify-between w-full h-20 py-8  lg:flex-wrap lg:justify-start lg:py-4
         transition ease-in-out duration-300
         ${isWhiteNav ? 'bg-white bg-opacity-85 shadow-md shadow-black/5' : ''}
         `}>
@@ -79,6 +85,47 @@ export default function RootLayout({
               회원가입
             </button>
           </div>
+        </div> */}
+
+        <div className={`fixed top-0 z-[90] flex items-center justify-center w-full h-20 py-4 flex-wrap md:flex-row md:flex-nowrap md:h-16 md:px-4 md:justify-between md:py-4 transition ease-in-out duration-300 ${isWhiteNav ? 'bg-white bg-opacity-85 shadow-md shadow-black/5' : ''}
+        md:bg-white md:bg-opacity-85 md:shadow-md md:shadow-black/5
+        
+        `}>
+          <div className="flex items-center w-[250px] ml-6 md:ml-0">
+            <Image src={'/images/logo.png'} alt='logo' width={250} height={150} />
+          </div>
+          <div className={`flex flex-row items-center gap-8 md:flex-col lg:gap-5 ml-auto mr-10 ${isMenuOpen ? 'md:flex' : 'md:hidden'}`}>
+            <Link className="text-xl font-semibold text-pro" href="#">
+              Pro
+            </Link>
+            <Link className={`text-xl font-semibold ${isWhiteNav ? "text-black" : "text-white"}`} href="#">
+              Info
+            </Link>
+            <Link className={`text-xl font-semibold ${isWhiteNav ? "text-black" : "text-white"}`} href="#">
+              구인
+            </Link>
+            <Link className={`text-xl font-semibold ${isWhiteNav ? "text-black" : "text-white"}`} href="#">
+              구직
+            </Link>
+            <Link className={`text-xl font-semibold ${isWhiteNav ? "text-black" : "text-white"}`} href="#">
+              로그인
+            </Link>
+            <button type="button" className={`text-xl ${isWhiteNav ? "text-black hover:bg-gray-100" : "text-white hover:bg-gray-600 hover:bg-opacity-20 "} border border-gray-300 focus:outline-none focus:ring-4 font-bold rounded-sm px-4 py-2.5 `}>
+              회원가입
+            </button>
+          </div>
+          {/* 햄버거 메뉴 아이콘 */}
+          <button className="hidden md:flex" onClick={toggleMenu}>
+            {isMenuOpen ? (
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            ) : (
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
+              </svg>
+            )}
+          </button>
         </div>
 
         {children}
--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ export default async function Home() {
 
 
 
-              <div className="z-20 text-right text-8xl">
+              <div className="z-10 text-right text-8xl">
                 BROWSE ARTIST<br />BID INSTANTLY
               </div>
 
