56c85b9bb069dd2c612458bfc25a2c542f11ef64
정재엽 <jjy37777@naver.com>
2024-05-12 02:45:27 +0900

    fix : undo PrelineScript

--- app/_components/PrelineScript.tsx
+++ app/_components/PrelineScript.tsx
@@ -1,58 +1,58 @@
-// "use client";
-
-// import { usePathname } from "next/navigation";
-// import { useEffect } from "react";
-
-// import { IStaticMethods } from "preline/preline";
-// declare global {
-//   interface Window {
-//     HSStaticMethods: IStaticMethods;
-//   }
-// }
-
-// export default function PrelineScript() {
-//   const path = usePathname();
-
-//   useEffect(() => {
-//     const loadPreline = async () => {
-//       await import("preline/preline");
-
-//       window.HSStaticMethods.autoInit();
-//     };
-
-//     loadPreline();
-//   }, [path]);
-
-//   return null;
-// }
 "use client";
 
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 
-const isBrowser = typeof window !== undefined; // check if component is rendered in a browser
+import { IStaticMethods } from "preline/preline";
+declare global {
+  interface Window {
+    HSStaticMethods: IStaticMethods;
+  }
+}
 
-export default function PrelineLoader() {
+export default function PrelineScript() {
   const path = usePathname();
 
   useEffect(() => {
-    if (isBrowser) {
-      // if this component is rendered on a browser, import preline
-      import("preline/preline");
-    }
-  }, [])
+    const loadPreline = async () => {
+      await import("preline/preline");
 
-  useEffect(() => {
-    setTimeout(() => {
-      if (isBrowser) {
-        // if this component is rendered on a browser, import relevant preline plugins
-        import("preline/preline").then(({ HSSelect, HSStepper}) => {
-          HSSelect.autoInit();
-          HSStepper.autoInit();
-        })
-      }
-    }, 100)
-  }, [path])
+      window.HSStaticMethods.autoInit();
+    };
 
-  return <></>
+    loadPreline();
+  }, [path]);
+
+  return null;
 }
+// "use client";
+
+// import { usePathname } from "next/navigation";
+// import { useEffect } from "react";
+
+// const isBrowser = typeof window !== undefined; // check if component is rendered in a browser
+
+// export default function PrelineLoader() {
+//   const path = usePathname();
+
+//   useEffect(() => {
+//     if (isBrowser) {
+//       // if this component is rendered on a browser, import preline
+//       import("preline/preline");
+//     }
+//   }, [])
+
+//   useEffect(() => {
+//     setTimeout(() => {
+//       if (isBrowser) {
+//         // if this component is rendered on a browser, import relevant preline plugins
+//         import("preline/preline").then(({ HSSelect, HSStepper}) => {
+//           HSSelect.autoInit();
+//           HSStepper.autoInit();
+//         })
+//       }
+//     }, 100)
+//   }, [path])
+
+//   return <></>
+// }
