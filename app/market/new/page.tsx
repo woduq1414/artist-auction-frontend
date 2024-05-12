3d76c3ab0657ea71bd70761a51f6dc789a56542c
정재엽 <jjy37777@naver.com>
2024-05-12 15:13:57 +0900

    feat : start price field add

--- app/market/new/page.tsx
+++ app/market/new/page.tsx
@@ -33,6 +33,7 @@ const NewMarketPage: React.FC = () => {
 
     const [title, setTitle] = useState<string>('');
     const [description, setDescription] = useState<string>('');
+    const [price, setPrice] = useState<number>(0);
 
 
     let firstCategoryRef = useRef<any>(null);
@@ -288,6 +289,61 @@ const NewMarketPage: React.FC = () => {
                                             </p>
                                         </div>
                                     </div>
+                                    <div className='flex flex-col gap-2'>
+                                        <label
+                                            htmlFor="inline-input-label-with-helper-text"
+                                            className="block text-lg font-semibold "
+                                        >
+                                            경매 시작가
+                                        </label>
+                                        <div className="relative max-w-xl ">
+                                            <input
+                                                type="text"
+                                                id="hs-input-with-leading-and-trailing-icon"
+                                                name="hs-input-with-leading-and-trailing-icon"
+                                                className="block w-full max-w-xl px-4 py-3 border border-gray-200 rounded-lg text-md ps-9 pe-16 focus:z-10 focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+                                                placeholder={price.toString()}
+                                                onChange={(e) => {
+                                                    const value: string = e.target.value;
+                                                    const removedCommaValue: number = Number(value.replaceAll(",", ""));
+                                                    if(removedCommaValue < 10000){
+                                                        setPrice(removedCommaValue);
+                                                    }
+                                                   
+                                                 }}
+                                                 value = {price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                                            />
+                                            <div className="absolute inset-y-0 z-20 flex items-center pointer-events-none start-0 ps-4">
+                                                <svg
+                                                    width={18}
+                                                    height={18}
+                                                    viewBox="0 0 24 24"
+                                                    fill="none"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    
+                                                >
+                                                    <path
+                                                        d="M6 12L8 19L10 12M6 12L4 5M6 12H3M6 12H10M14 12L16 19L18 12M14 12L12 5L10 12M14 12H10M14 12H18M18 12L20 5M18 12H21"
+                                                        stroke="#444444"
+                                                        strokeWidth={2}
+                                                        strokeLinecap="round"
+                                                        strokeLinejoin="round"
+                                                    />
+                                                </svg>
+
+                                            </div>
+                                            <div className="absolute inset-y-0 z-20 flex items-center pointer-events-none end-0 pe-4">
+                                                <span className="text-gray-500 dark:text-neutral-500">만원</span>
+                                            </div>
+                                        </div>
+
+                                        <p
+                                            className="text-gray-500 text-md dark:text-neutral-500"
+                                            id="hs-inline-input-helper-text"
+                                        >
+                                            10,000원 단위로만 입력 가능합니다.
+                                        </p>
+                                    </div>
                                 </div>
 
 
@@ -338,69 +394,69 @@ const NewMarketPage: React.FC = () => {
                             </div>
                             {/* End Final Contnet */}
                             {/* Button Group */}
-                            
+
                             {/* End Button Group */}
                         </div>
                         <div className="flex items-center justify-between flex-shrink-0 h-[3rem] mt-5 gap-x-2 mx-5 my-5">
-                                <button
-                                    type="button"
-                                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-gray-800 bg-white border border-gray-200 rounded-lg shadow-sm gap-x-1 hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
-                                    data-hs-stepper-back-btn=""
+                            <button
+                                type="button"
+                                className="inline-flex items-center px-3 py-2 text-sm font-medium text-gray-800 bg-white border border-gray-200 rounded-lg shadow-sm gap-x-1 hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none"
+                                data-hs-stepper-back-btn=""
+                            >
+                                <svg
+                                    className="flex-shrink-0 size-4"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width={24}
+                                    height={24}
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth={2}
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
                                 >
-                                    <svg
-                                        className="flex-shrink-0 size-4"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width={24}
-                                        height={24}
-                                        viewBox="0 0 24 24"
-                                        fill="none"
-                                        stroke="currentColor"
-                                        strokeWidth={2}
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                    >
-                                        <path d="m15 18-6-6 6-6" />
-                                    </svg>
-                                    이전
-                                </button>
-                                <button
-                                    type="button"
-                                    className="inline-flex items-center px-3 py-2 text-sm font-semibold text-white border border-transparent rounded-lg bg-primary gap-x-1 hover:bg-primary disabled:opacity-50 disabled:pointer-events-none"
-                                    data-hs-stepper-next-btn=""
+                                    <path d="m15 18-6-6 6-6" />
+                                </svg>
+                                이전
+                            </button>
+                            <button
+                                type="button"
+                                className="inline-flex items-center px-3 py-2 text-sm font-semibold text-white border border-transparent rounded-lg bg-primary gap-x-1 hover:bg-primary disabled:opacity-50 disabled:pointer-events-none"
+                                data-hs-stepper-next-btn=""
+                            >
+                                다음
+                                <svg
+                                    className="flex-shrink-0 size-4"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width={24}
+                                    height={24}
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth={2}
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
                                 >
-                                    다음
-                                    <svg
-                                        className="flex-shrink-0 size-4"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width={24}
-                                        height={24}
-                                        viewBox="0 0 24 24"
-                                        fill="none"
-                                        stroke="currentColor"
-                                        strokeWidth={2}
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                    >
-                                        <path d="m9 18 6-6-6-6" />
-                                    </svg>
-                                </button>
-                                <button
-                                    type="button"
-                                    className="inline-flex items-center px-3 py-2 text-sm font-semibold text-white border border-transparent rounded-lg bg-primary gap-x-1 hover:bg-primary disabled:opacity-50 disabled:pointer-events-none"
-                                    data-hs-stepper-finish-btn=""
-                                    style={{ display: "none" }}
-                                >
-                                    제출
-                                </button>
-                                <button
-                                    type="reset"
-                                    className="inline-flex items-center px-3 py-2 text-sm font-semibold text-white border border-transparent rounded-lg bg-primary gap-x-1 hover:bg-primary disabled:opacity-50 disabled:pointer-events-none"
-                                    data-hs-stepper-reset-btn=""
-                                    style={{ display: "none" }}
-                                >
-                                    Reset
-                                </button>
-                            </div>
+                                    <path d="m9 18 6-6-6-6" />
+                                </svg>
+                            </button>
+                            <button
+                                type="button"
+                                className="inline-flex items-center px-3 py-2 text-sm font-semibold text-white border border-transparent rounded-lg bg-primary gap-x-1 hover:bg-primary disabled:opacity-50 disabled:pointer-events-none"
+                                data-hs-stepper-finish-btn=""
+                                style={{ display: "none" }}
+                            >
+                                제출
+                            </button>
+                            <button
+                                type="reset"
+                                className="inline-flex items-center px-3 py-2 text-sm font-semibold text-white border border-transparent rounded-lg bg-primary gap-x-1 hover:bg-primary disabled:opacity-50 disabled:pointer-events-none"
+                                data-hs-stepper-reset-btn=""
+                                style={{ display: "none" }}
+                            >
+                                Reset
+                            </button>
+                        </div>
                         {/* End Stepper Content */}
                     </div>
                     {/* End Stepper */}
