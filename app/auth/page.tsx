cdd2f6bcbc58fa710194539535edac136f12ce10
정재엽 <jjy37777@naver.com>
2024-05-18 01:33:45 +0900

    feat : remove kakao script tag

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -206,7 +206,6 @@ const AuthPage: React.FC = () => {
 
 
             </div>
-            <script src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.1/kakao.min.js" integrity="sha384-kDljxUXHaJ9xAb2AzRd59KxjrFjzHa5TAoFQ6GbYTCAG0bjM55XohjjDT7tDDC01" crossOrigin="anonymous"></script>
         </main>
     );
 };
