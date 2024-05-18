ed84ac18b46a26951d8284373aae813c474a3810
정재엽 <jjy37777@naver.com>
2024-05-18 23:41:12 +0900

    feat : update prod url

--- config/config.production.ts
+++ config/config.production.ts
@@ -1,7 +1,7 @@
 import getConfigs from "./config.common";
  
 // 환경마다 달라져야 할 변수, 값들을 정의합니다. (여기는 production 환경에 맞는 값을 지정합니다.)
-const baseUrl = 'http://localhost.prod:3000';
+const baseUrl = 'https://api.artistauction.kro.kr/api/v1';
 const mode = 'production';
  
 // 환경마다 달라져야 할 값들을 getConfig 함수에 전달합니다.
