b1b690aae15a04b9f093456e0d133aeb883c487d
정재엽 <jjy37777@naver.com>
2024-06-19 22:23:26 +0900

    feat : category list navbar

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { EllipsisVerticalIcon, MagnifyingGlassIcon } from '@heroicons/react/24/solid'
 import CategoryList from "./_components/CategoryList";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAuth } from "./_store/useAuth";
 import { set } from "lodash";
 
@@ -24,22 +24,30 @@ import { set } from "lodash";
 // }
 
 
-export default  function Home() {
+export default function Home() {
 
-  
+
   // on scroll event
 
-  
+  const [isCategoryBarShow, setIsCategoryBarShow ] = useState(false);
+  let categoryListRef = useRef(null);
   const { setIsNavSearchBarShow } = useAuth();
   useEffect(() => {
     setIsNavSearchBarShow(false);
     window.addEventListener('scroll', () => {
       const getTop = (el: { offsetTop: any; offsetParent: any; }): number => el.offsetTop + (el.offsetParent && getTop(el.offsetParent))
       const searchBar = document.getElementById('mainPageSearchBar');
-      if(searchBar && window.scrollY > getTop(searchBar)){
+      let searchBarTop = getTop(searchBar as any);
+      if (searchBar && window.scrollY > searchBarTop) {
         setIsNavSearchBarShow(true);
-      }else{
+      } else {
         setIsNavSearchBarShow(false);
+      }
+
+      if (categoryListRef.current && window.scrollY > (categoryListRef.current as HTMLElement).offsetTop - 300) {
+        setIsCategoryBarShow(true);
+      }else{
+        setIsCategoryBarShow(false);
       }
     });
   }, []);
@@ -47,9 +55,22 @@ export default  function Home() {
 
   return (
     <main className="flex flex-col items-center justify-between min-h-screen">
+      <div className={`h-[50px] sticky top-[0px]  w-full text-gray-700 bg-white border-b-2 z-[100] transition duration-500 flex flex-row items-center px-10 gap-[80px]
+        ${isCategoryBarShow ? "translate-y-[80px] opacity-100" : "translate-y-[0px] opacity-0"}
+        
+        `}>
+        {
+          ["Design", "Illustration", "Animation"].map((category, index) => (
+            <div className="text-lg">
+              {category}
+            </div>
+          ))
+        }
+      </div>
       <div
-        className="h-[80px] md:h-[64px]"
+        className="h-[30px] md:h-[14px]"
       />
+
 
       <div className="w-[calc(100%-72px)] h-[calc(100vh-80px-32px)]  mx-10 mt-4  my-auto relative">
         <img src="/images/main_image.jpg" alt="hero"
@@ -73,7 +94,7 @@ export default  function Home() {
           </div>
         </div>
       </div>
-      <div className="mt-4 h-[500px] bg-primary">
+      <div className="mt-4 h-[5000px] bg-primary" ref={categoryListRef}>
         안녕하세요
       </div>
 
